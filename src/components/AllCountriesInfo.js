1324f58f188bea28c82e4b04cc3a3d6fc15ed278
harrshini12 <harrshini.1995@gmail.com>
2023-06-29 10:20:18 +0200

    added loading message

--- src/components/AllCountriesInfo.js
+++ src/components/AllCountriesInfo.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const AllCountriesInfo = () => {
   const [countriesInfo, setCountriesInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchCountriesInfo = async () => {
@@ -11,8 +12,10 @@ const AllCountriesInfo = () => {
           'http://localhost:3000/allcountriesinfo',
         );
         setCountriesInfo(response.data);
+        setIsLoading(false); 
       } catch (error) {
         console.error(error);
+        setIsLoading(false); 
       }
     };
 
@@ -22,28 +25,32 @@ const AllCountriesInfo = () => {
   return (
     <div className="m-5">
       <h2 className="my-4 text-center">All Countries Info</h2>
-      <table className="table my-4">
-        <thead>
-          <tr>
-            <th>Country</th>
-            <th>Cases</th>
-            <th>Deaths</th>
-            <th>Today's Cases</th>
-            <th>Today's Deaths</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countriesInfo.map((country) => (
-            <tr key={country.country}>
-              <td>{country.country}</td>
-              <td>{country.cases}</td>
-              <td>{country.deaths}</td>
-              <td>{country.todayCases}</td>
-              <td>{country.todayDeaths}</td>
+      {isLoading ? (
+        <p>Loading all countries info...</p>
+      ) : (
+        <table className="table my-4">
+          <thead>
+            <tr>
+              <th>Country</th>
+              <th>Cases</th>
+              <th>Deaths</th>
+              <th>Today's Cases</th>
+              <th>Today's Deaths</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {countriesInfo.map((country) => (
+              <tr key={country.country}>
+                <td>{country.country}</td>
+                <td>{country.cases}</td>
+                <td>{country.deaths}</td>
+                <td>{country.todayCases}</td>
+                <td>{country.todayDeaths}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
